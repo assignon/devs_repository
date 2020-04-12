4b51413c73eab1c4cdb5d4bb68015151cfeee2cf
yanick Assignon <yanick.py@localhost.localdomain>
2020-04-12 23:56:10 +0200

    django and vue configuration done

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_PARTY_APPS = [
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+]
+
+LOCAL_APPS = [
+
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -51,10 +63,17 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'portfolio.urls'
 
+# Vue project location
+FRONTEND_DIR = os.path.join(BASE_DIR, 'frontend')
+# Vue assets directory (assetsDir)
+STATICFILES_DIRS = [
+    os.path.join(FRONTEND_DIR, 'dist/static'),
+]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(FRONTEND_DIR, 'dist'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +137,38 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# MEDIA_ROOT = str(APPS_DIR('media'))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated', )
+}
+
+# CSRF_COOKIE_NAME = "csrftoken"
+CSRF_COOKIE_NAME = "XSRF-TOKEN"
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_EXPOSE_HEADERS = (
+    'Access-Control-Allow-Origin: *',
+)
+
+CORS_ORIGIN_WHITELIST = (
+    # 'localhost:8080',
+    # '192.168.56.1:8080/',
+)
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,8 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.urls import include, path
+from django.conf.urls.static import static
+# from django.views.generic import TemplateView
+# from django.views import defaults as default_views
+# from django.contrib.auth.views import logout
+from .router import router
+from portfolio import views
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
-]
+    path('api/', include(router.urls)),
+    # path('products/', include("products.urls", namespace="products")),
+    # frontend urls
+    # path('', views.index, name='index'),
+    # path('about/', views.about, name='about'),
+    # path('product/<str:id>/', views.product, name='product'),
+    # path('signup/', views.signup, name='signup'),
+    # path('contact/', views.contact, name='contact'),
+
+]+ static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT,
+    # settings.STATIC_URL, document_root=settings.STATIC_ROOT
+)
