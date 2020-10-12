60de5b8ff8e3b0a321bcf5f89a7f90579abf2517
Yanick Assignon <assignon>
2020-10-13 00:23:24 +0200

    site titel updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -187,5 +187,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_USER_TLS = True
+EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
